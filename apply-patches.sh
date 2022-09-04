83e6d335409ee273b44464d255d9a1b6f9a32ac4
Martin F. Schumann <mfs@mfs.name>
2022-09-04 20:14:03 +0200

    Download external patches

--- apply-patches.sh
+++ apply-patches.sh
@@ -11,10 +11,22 @@ find thunderbird-patches/$VERSION -type f -name *.patch -exec cp '{}' patches ';
 
 echo
 echo "======================================================="
+echo "Downloading upstream patches for main repository"
+echo '#!/bin/bash' > external.sh
+grep " # " thunderbird-patches/$VERSION/series-M-C >> external.sh
+sed -i -e 's/\/rev\//\/raw-rev\//' external.sh
+sed -i -e 's/\(.*\) # \(.*\)/wget -nc \2 -O patches\/\1/' external.sh
+chmod 700 external.sh
+./external.sh
+rm -f external.sh
+
+echo
+echo "======================================================="
 echo "Applying patch series for main repository"
 echo "... without patches for Windows installer"
 sed -i 's/08-branding-m-c.patch/# 08-branding-m-c.patch/g' thunderbird-patches/$VERSION/series-M-C
 sed -i 's/08a-branding-m-c.patch/# 08a-branding-m-c.patch/g' thunderbird-patches/$VERSION/series-M-C
+
 cat thunderbird-patches/$VERSION/series-M-C | while read line || [[ -n $line ]]
     do 
         if [[ -f patches/$line ]]
@@ -23,6 +35,17 @@ cat thunderbird-patches/$VERSION/series-M-C | while read line || [[ -n $line ]]
             git apply --stat --apply patches/$line
         fi
     done
+
+echo
+echo "======================================================="
+echo "Downloading upstream patches for comm repository"
+echo '#!/bin/bash' > external.sh
+grep " # " thunderbird-patches/$VERSION/series >> external.sh
+sed -i -e 's/\/rev\//\/raw-rev\//' external.sh
+sed -i -e 's/\(.*\) # \(.*\)/wget -nc \2 -O patches\/\1/' external.sh
+chmod 700 external.sh
+./external.sh
+rm -f external.sh
 
 echo
 echo "======================================================="
