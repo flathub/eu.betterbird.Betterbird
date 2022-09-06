c4ad6ffcd358fc14785d95adcd52ffa9bb31249b
Martin Schumann <mfs@mfs.name>
2022-09-06 11:41:37 +0200

    moved external patches to udpate-version

--- apply-patches.sh
+++ apply-patches.sh
@@ -11,17 +11,6 @@ find thunderbird-patches/$VERSION -type f -name *.patch -exec cp '{}' patches ';
 
 echo
 echo "======================================================="
-echo "Downloading upstream patches for main repository"
-echo '#!/bin/bash' > external.sh
-grep " # " thunderbird-patches/$VERSION/series-M-C >> external.sh
-sed -i -e 's/\/rev\//\/raw-rev\//' external.sh
-sed -i -e 's/\(.*\) # \(.*\)/wget -nc \2 -O patches\/\1/' external.sh
-chmod 700 external.sh
-./external.sh
-rm -f external.sh
-
-echo
-echo "======================================================="
 echo "Applying patch series for main repository"
 echo "... without patches for Windows installer"
 sed -i 's/08-branding-m-c.patch/# 08-branding-m-c.patch/g' thunderbird-patches/$VERSION/series-M-C
@@ -29,23 +18,13 @@ sed -i 's/08a-branding-m-c.patch/# 08a-branding-m-c.patch/g' thunderbird-patches
 
 cat thunderbird-patches/$VERSION/series-M-C | while read line || [[ -n $line ]]
     do 
-        if [[ -f patches/$line ]]
+        patch=$(echo $line | cut -f1 -d'#')
+        if [[ -f patches/$patch ]]
         then
-            echo Applying patch $line ... 
-            git apply --stat --apply patches/$line
+            echo Applying patch $patch ... 
+            git apply --stat --apply patches/$patch
         fi
     done
-
-echo
-echo "======================================================="
-echo "Downloading upstream patches for comm repository"
-echo '#!/bin/bash' > external.sh
-grep " # " thunderbird-patches/$VERSION/series >> external.sh
-sed -i -e 's/\/rev\//\/raw-rev\//' external.sh
-sed -i -e 's/\(.*\) # \(.*\)/wget -nc \2 -O patches\/\1/' external.sh
-chmod 700 external.sh
-./external.sh
-rm -f external.sh
 
 echo
 echo "======================================================="
@@ -53,10 +32,11 @@ echo "Applying patch series for comm repository"
 cd comm
 cat ../thunderbird-patches/$VERSION/series | while read line || [[ -n $line ]]
     do
-        if [[ -f ../patches/$line ]]
+        patch=$(echo $line | cut -f1 -d'#')
+        if [[ -f ../patches/$patch ]]
         then
-            echo Applying patch $line ... 
-            git apply --stat --apply ../patches/$line
+            echo Applying patch $patch ... 
+            git apply --stat --apply ../patches/$patch
         fi
     done
 cd ..
