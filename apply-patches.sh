97e55a30a372750d95ede60a54770910206ab731
Martin Schumann <mfs@mfs.name>
2022-09-09 01:02:14 +0200

    Patch lang packs
    silence unzip, fix typo
    
    
    silence zip
    
    
    forgot to cd back, clean up
    
    
    replace \ -> /, line endings
    
    
    accept REM for comments

--- apply-patches.sh
+++ apply-patches.sh
@@ -51,3 +51,47 @@ cat ../thunderbird-patches/$VERSION/series | while read line || [[ -n $line ]]
         fi
     done
 cd ..
+
+echo
+echo "======================================================="
+echo "Patching language packs"
+cd langpacks
+for langpack in *.xpi
+do 
+    lang=$(echo "$langpack" | sed -r 's#\langpack-([^@]+)@thunderbird.mozilla.org.xpi#\1#')
+    echo " -- $lang --"
+    mkdir $lang
+    cd $lang
+
+    echo "   * extracting original lang pack"
+    unzip -q ../$langpack
+    rm -f ../$langpack
+
+    echo "   * removing original branding"
+    rm -f chrome/$lang/locale/$lang/branding/*
+    rm -f localization/$lang/branding/*
+
+    echo "   * modifying manifest.json"
+    sed -i -e 's/@thunderbird.mozilla.org/@betterbird.eu/' manifest.json
+    sed -i -e 's/Language pack for Thunderbird/Language pack for Betterbird/' manifest.json
+
+    echo "   * copying Betterbird branding from en-US"
+    branding_source="../../comm/mail/branding/betterbird/locales/en-US"
+    cp "$branding_source/brand.ftl" localization/$lang/branding/
+    cp "$branding_source/brand.dtd" "$branding_source/brand.properties" chrome/$lang/locale/$lang/branding/ 
+
+    bb_string_patcher="../../thunderbird-patches/$VERSION/scripts/$lang.cmd"
+    if [[ -f "$bb_string_patcher" ]]
+    then
+        echo "   * adding extra strings"
+        sed -ri 's/^(::|REM)/#/; s/%lecho%/lessecho/; s/\r$//' "$bb_string_patcher"
+        perl -pi -e 's#\\(?=[^ ]+$)#/#g' "$bb_string_patcher"
+        . "$bb_string_patcher"
+    fi
+
+    echo "   * packing modified language pack"
+    zip -qr "../langpack-$lang@betterbird.eu.xpi" manifest.json chrome localization
+
+    cd ..
+    rm -rf $lang
+done
