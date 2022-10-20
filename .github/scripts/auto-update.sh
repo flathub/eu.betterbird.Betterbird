c041b06b1d0d94f5bc77cacbc487e273bf5332a1
Martin F. Schumann <mfs@mfs.name>
2022-10-20 20:27:20 +0200

    Update readme and auto-update script (#45)

--- .github/scripts/auto-update.sh
+++ .github/scripts/auto-update.sh
@@ -13,18 +13,25 @@ then
     if [[ $(echo $tag | cut -f1 -d.) == $auto_update_major_release ]]
     then
       target_tag=$tag
+      update_branch=$(git ls-remote --heads origin update-$target_tag)
     fi
   done
   if [[ -n $target_tag ]]
   then
-    echo " --- Updating to $target_tag ---"
-    git config user.name github-actions
-    git config user.email github-actions@github.com
-    ./update-version.sh $target_tag \
-    && echo "${all_tags}" > .known-tags \
-    && git commit -m "Update to $target_tag" -- .known-tags eu.betterbird.Betterbird.json thunderbird-sources.json .build-date \
-    || exit 1
+    if [[ -z $update_branch ]]
+    then
+      echo " --- Updating to $target_tag ---"
+      ./update-version.sh $target_tag \
+      && echo "${all_tags}" > .known-tags \
+      || exit 1
+      echo "version_updated=true" >> $GITHUB_ENV
+      echo "new_version=$target_tag" >> $GITHUB_ENV
+    else
+      echo " --- Skipping update, because branch update-$target_tag already exists ---"
+      echo "version_updated=false" >> $GITHUB_ENV
+    fi
   fi
 else
   echo " --- No new tags found ---"
+  echo "version_updated=false" >> $GITHUB_ENV
 fi
