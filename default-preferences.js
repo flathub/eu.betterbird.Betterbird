a28484c9dec0e1e0a80eba3aee071d91b4f66dd3
Martin Schumann <mfs@mfs.name>
2023-07-06 18:40:57 +0200

    Revert shipping langpacks as system extensions

--- default-preferences.js
+++ default-preferences.js
@@ -6,4 +6,3 @@ pref("intl.locale.matchOS", true);
 pref("mail.shell.checkDefaultClient", false);
 pref("mail.shell.checkDefaultMail", false);
 pref("spellchecker.dictionary_path", "/usr/share/hunspell");
-pref("extensions.autoDisableScopes", 3);
