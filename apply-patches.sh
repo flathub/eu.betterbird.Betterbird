33a5183966bfd328e0a2eb918064aa5ce83e7658
Martin Schumann <mfs@mfs.name>
2024-07-02 21:59:40 +0200

    fixed apply-patches.sh

--- apply-patches.sh
+++ apply-patches.sh
@@ -6,7 +6,9 @@ VERSION="$1"
 echo
 echo "======================================================="
 echo "Copying patches"
-find thunderbird-patches/$VERSION -type f -name *.patch -exec cp '{}' patches ';'
+rm -rf patches
+mkdir patches
+find thunderbird-patches/$VERSION -type f -name *.patch -exec cp '{}' patches/ ';'
 
 echo
 echo "======================================================="
