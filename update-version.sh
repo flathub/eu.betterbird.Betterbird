5ef27332de141a8ceea9af6df5caa0f048c230b8
Martin F. Schumann <mfs@mfs.name>
2025-06-26 18:54:36 +0200

    Update to 140.0esr-bb4 (#220)
    * make url parsing more robust, update to 140.0esr-bb4
    
    * update sources

--- update-version.sh
+++ update-version.sh
@@ -158,7 +158,7 @@ while read -r line; do
     $SOURCES_FILE > $tmpfile
   mv $tmpfile $SOURCES_FILE
   rm -f $name
-done < <(grep -E "^[^#].* # " thunderbird-patches/$(echo $BETTERBIRD_VERSION | cut -f1 -d'.')/series-moz)
+done < <(grep -E "^[^#].* # +http" thunderbird-patches/$(echo $BETTERBIRD_VERSION | cut -f1 -d'.')/series-moz)
 # patch series for comm repo
 while read -r line; do
   url=$(echo $line | sed -r 's/(.*) # (http.*\/rev\/[0-9a-f]+).*/\2/' | sed -e 's/\/rev\//\/raw-rev\//')
@@ -170,7 +170,8 @@ while read -r line; do
     $SOURCES_FILE > $tmpfile
   mv $tmpfile $SOURCES_FILE
   rm -f $name
-done < <(grep -E "^[^#].* # " thunderbird-patches/$(echo $BETTERBIRD_VERSION | cut -f1 -d'.')/series)
+done < <(grep -E "^[^#].* # +http" thunderbird-patches/$(echo $BETTERBIRD_VERSION | cut -f1 -d'.')/series)
+rm -rf thunderbird-patches
 
 # add tag to .known-tags if it has not been added yet 
 if [[ "$source_spec" == "tag" ]] && ! grep -Fxq "$BETTERBIRD_VERSION" "$KNOWN_TAGS_FILE"
